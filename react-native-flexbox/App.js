dec6fd52ffda207b91499dd79a5c5780144cc6fb
ker-mari <magmayoker@gmail.com>
2026-03-14 04:00:53 +0800

    Done with Flex section

---
+++ react-native-flexbox/App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Flex from './components/Flex';
+
+export default function App() {
+  return (
+    <Flex />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
