d8152b1c08ce1bd23613ea189f4d6c5864118d8d
ker-mari <magmayoker@gmail.com>
2026-03-14 04:03:58 +0800

    Done with DirectionLayout

--- react-native-flexbox/App.js
+++ react-native-flexbox/App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Flex from './components/Flex';
 import FlexDirectionBasics from './components/FlexDirectionBasics';
+import DirectionLayout from './components/DirectionLayout';
 
 export default function App() {
   return (
     // <Flex />
-    <FlexDirectionBasics />
+    // <FlexDirectionBasics />
+    <DirectionLayout />
   );
 }
 
