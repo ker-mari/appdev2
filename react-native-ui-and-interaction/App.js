5e0e731b898bc24d6a02843635f28ee55949d7c4
ker-mari <magmayoker@gmail.com>
2026-03-13 23:02:57 +0800

    Done with percetange

--- react-native-ui-and-interaction/App.js
+++ react-native-ui-and-interaction/App.js
@@ -2,8 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import Style from './components/Style';
 // import FixedDimensionsBasics from './components/FixedDimensionsBasics';
-import FlexDimensionsBasics from './components/FlexDimensionsBasics';
-// import PercentageDimensionsBasics from './components/PercentageDimensionsBasics';
+// import FlexDimensionsBasics from './components/FlexDimensionsBasics';
+import PercentageDimensionsBasics from './components/PercentageDimensionsBasics';
 // import FixedDimensionsBasics from './components/FixedDimensionBasics';
 
 export default function App() {
@@ -14,8 +14,8 @@ export default function App() {
     //   <FlexDimensionsBasics/>
     // </ScrollView>
     <>
-      <FlexDimensionsBasics/>
-      {/* <PercentageDimensionsBasics/> */}
+      {/* <FlexDimensionsBasics/> */}
+      <PercentageDimensionsBasics/>
       {/* <FixedDimensionsBasics /> */}
     </>
   );
