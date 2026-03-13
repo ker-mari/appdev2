ad816ffe686fab0671bc95b359f92b16429f38f2
ker-mari <magmayoker@gmail.com>
2026-03-13 23:01:55 +0800

    Done with fixed dimensions

--- react-native-ui-and-interaction/App.js
+++ react-native-ui-and-interaction/App.js
@@ -1,17 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import WelcomeScreen from './src/screens/WelcomeScreen';
-import LoginScreen from './src/screens/LoginScreen';
-import SignupScreen from './src/screens/SignupScreen';
-import TodoScreen from './src/screens/TodoScreen';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import Style from './components/Style';
+// import FixedDimensionsBasics from './components/FixedDimensionsBasics';
+// import FlexDimensionsBasics from './components/FlexDimensionsBasics';
+// import PercentageDimensionsBasics from './components/PercentageDimensionsBasics';
+import FixedDimensionsBasics from './components/FixedDimensionBasics';
 
 export default function App() {
   return (
+    // <ScrollView contentContainerStyle={styles.container}>
+      // <Style/>
+    //   <FixedDimensionsBasics/>
+    //   <FlexDimensionsBasics/>
+    // </ScrollView>
     <>
-      {/* <WelcomeScreen/> */}
-      {/* <LoginScreen/> */}
-      {/* <SignupScreen/> */}
-      <TodoScreen/>
+      {/* <FlexDimensionsBasics/> */}
+      {/* <PercentageDimensionsBasics/> */}
+      <FixedDimensionsBasics />
     </>
   );
 }
@@ -22,5 +27,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 40
   },
 });
