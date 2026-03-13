1add8499ce2b6eab3027a7d9b62e18ec14bd5a3f
ker-mari <magmayoker@gmail.com>
2026-03-14 04:05:31 +0800

    Done with Justify content baiscs

--- react-native-flexbox/App.js
+++ react-native-flexbox/App.js
@@ -3,12 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import Flex from './components/Flex';
 import FlexDirectionBasics from './components/FlexDirectionBasics';
 import DirectionLayout from './components/DirectionLayout';
+import JustifyContentBasics from './components/JustifyContentBasics';
 
 export default function App() {
   return (
     // <Flex />
     // <FlexDirectionBasics />
-    <DirectionLayout />
+    // <DirectionLayout />
+    <JustifyContentBasics />
   );
 }
 
